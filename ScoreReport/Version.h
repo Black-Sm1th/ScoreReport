2d8f3f198ffb38438e112cf8f9593f7d5f922112
Black-Sm1th <714557144@qq.com>
2025-09-09 11:42:23 +0800

    feat：新增版本文件

---
+++ ScoreReport/Version.h
@@ -0,0 +1,26 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+// 版本信息定义
+#define VER_MAJOR 0
+#define VER_MINOR 8
+#define VER_PATCH 1
+#define VER_BUILD 0
+
+// 版本字符串(统一使用一个版本号)
+#define VER_VERSION_STR "0.8.1"
+#define VER_PRODUCTVERSION_STR "0.8.1"
+
+// 产品信息
+#define VER_COMPANYNAME_STR "AETHERMIND"
+#define VER_FILEDESCRIPTION_STR "ScoreReport - 医学评分报告系统"
+#define VER_INTERNALNAME_STR "ScoreReport"
+#define VER_LEGALCOPYRIGHT_STR "Copyright © 2024 AETHERMIND. All rights reserved."
+#define VER_ORIGINALFILENAME_STR "ScoreReport.exe"
+#define VER_PRODUCTNAME_STR "ScoreReport"
+
+// 文件类型
+#define VER_FILETYPE VFT_APP
+#define VER_FILESUBTYPE VFT2_UNKNOWN
+
+#endif // VERSION_H
