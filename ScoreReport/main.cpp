648028c6bece3a6f6bf535468f91e26e326f4362
Black-Sm1th <714557144@qq.com>
2025-09-26 17:42:41 +0800

    fix：修改版本号

--- ScoreReport/main.cpp
+++ ScoreReport/main.cpp
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationName("AETHERMIND");
     QCoreApplication::setOrganizationDomain("aethermind.com");
     QCoreApplication::setApplicationName("ScoreReport");
-    QCoreApplication::setApplicationVersion("0.9.1");
+    QCoreApplication::setApplicationVersion("0.9.3");
 
     QQmlApplicationEngine engine;
     // 使用单例模式获取实例
