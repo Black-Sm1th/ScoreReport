5efdda43d364112a03ebff7859469415085c7cb8
Black-Sm1th <714557144@qq.com>
2025-10-16 10:48:52 +0800

    fix：修改版本号

--- ScoreReport/Version.h
+++ ScoreReport/Version.h
@@ -4,12 +4,12 @@
 // 版本信息定义
 #define VER_MAJOR 0
 #define VER_MINOR 10
-#define VER_PATCH 0
+#define VER_PATCH 1
 #define VER_BUILD 0
 
 // 版本字符串(统一使用一个版本号)
-#define VER_VERSION_STR "0.10.0"
-#define VER_PRODUCTVERSION_STR "0.10.0"
+#define VER_VERSION_STR "0.10.1"
+#define VER_PRODUCTVERSION_STR "0.10.1"
 
 // 产品信息
 #define VER_COMPANYNAME_STR "AETHERMIND"
